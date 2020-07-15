69a5dfd0188fe2d0cbd658c0400226f62505f565
Cody Morley <codymorleyinc@gmail.com>
2020-07-15 08:58:08 -0700

    almost there
    grrrr

--- SpaceAgeObjC/SpaceAge.h
+++ SpaceAgeObjC/SpaceAge.h
@@ -6,4 +6,27 @@
 //  Copyright © 2019 joshua kaunert. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+@interface SpaceAge : NSObject
+
+@property (nonatomic) double age;
+@property (nonatomic, readonly) double onMercury;
+@property (nonatomic, readonly) double onVenus;
+@property (nonatomic, readonly) double onMars;
+@property (nonatomic, readonly) double onJupiter;
+@property (nonatomic, readonly) double onSaturn;
+@property (nonatomic, readonly) double onUranus;
+@property (nonatomic, readonly) double onNeptune;
+
+
+- (instancetype)initWithSeconds:(int)seconds;
+
+- (NSString *)yearsAge:(double)age
+                planet:(string)planet;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
--- SpaceAgeObjC/SpaceAge.m
+++ SpaceAgeObjC/SpaceAge.m
@@ -7,4 +7,60 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SpaceAge.h"
 
+
+@implementation SpaceAge
+
+- (instancetype)initWithSeconds:(int)seconds
+{
+    self = [super init];
+    if (self) {
+        _age = seconds;
+    }
+    return self;
+}
+
+
+- (double)onMercury
+{
+    return self.age * 0.2408467;
+}
+
+- (double)onVenus
+{
+    return self.age * 0.61519726;
+}
+
+- (double)onMars
+{
+    return self.age * 1.8808158;
+}
+
+- (double)onJupiter
+{
+    return self.age * 11.862615;
+}
+
+- (double)onSaturn
+{
+    return self.age * 29.447498;
+}
+
+- (double)onUranus
+{
+    return self.age * 84.016846;
+}
+
+- (double)onNeptune
+{
+    return self.age * 164.79132;
+}
+
+-(NSString *)yearsAge:(double)age
+{
+    
+}
+
+
+@end
